5f8807c7076b7674941b91c36720f1bf2a86ca07
shark <sharkshore@163.com>
2018-04-14 17:47:08 +0800

    修复controller

--- app-service/src/main/java/com/shark/mybatisboot/service/DemoService.java
+++ app-service/src/main/java/com/shark/mybatisboot/service/DemoService.java
@@ -2,6 +2,7 @@ package com.shark.mybatisboot.service;
 
 import com.shark.mybatisboot.dal.dao.DemoDao;
 import com.shark.mybatisboot.dal.mapper.DemoMapper;
+import com.shark.mybatisboot.dal.model.Demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,7 @@ import java.util.Map;
  * Created by tuze on 2017/8/23.
  */
 @Service
-public class DemoService implements IDemoService {
+public class DemoService {
 
     @Autowired
     private DemoMapper demoMapper;
@@ -24,7 +25,7 @@ public class DemoService implements IDemoService {
         return demoMapper.queryMemberName();
     }
 
-    public  List<Map<String, Object>>   querySqlModel(){
+    public  List<Map<String, Object>>   selectBySelective(Demo demo){
         return demoDao.querySqlModel();
     }
 
